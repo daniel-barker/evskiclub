f9a7807dee6e5686da5b224d3ffb431461c1ce50
dan barker <dbarker16@gmail.com>
2024-02-05 12:19:29 -0500

    removed members api

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,4 +4,3 @@ export const IMAGES_URL = "/api/images";
 export const EVENTS_URL = "/api/events";
 export const UPLOAD_URL = "/api/upload";
 export const NEWS_URL = "/api/news";
-export const MEMBERS_URL = "/api/members";
