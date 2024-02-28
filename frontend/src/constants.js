7d1360e73e7443411726699df49fc99ee52bd637
dan barker <dbarker16@gmail.com>
2024-02-27 19:42:30 -0500

    Redux slice setup

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,3 +3,4 @@ export const USERS_URL = "/api/users";
 export const IMAGES_URL = "/api/images";
 export const EVENTS_URL = "/api/events";
 export const NEWS_URL = "/api/news";
+export const UNIT_URL = "/api/unit";
